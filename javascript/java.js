8faee998b9b943efa5f84ed2def76c4936de85d8
Shawn9717 <shawn.njoga@student.moringaschool.com>
2021-09-14 10:31:45 +0300

    change readme

--- javascript/java.js
+++ javascript/java.js
@@ -15,20 +15,7 @@ $(document).ready(function() {
 });
 
 // For hover effect on PORTFOLIO
-$(document).ready(function() {
-    $("div.image-frame").hover(function() {
-        $(".classAdd1").addClass("mask flex-center rgba-red-strong");
-    });
-    $("div.image-frame").hover(function() {
-        $(".classAdd2").addClass("mask flex-center rgba-blue-light");
-    });
-    $("div.image-frame").hover(function() {
-        $(".classAdd3").addClass("mask flex-center rgba-green-slight");
-    });
-    $("div.image-frame").hover(function() {
-        $(".classAdd4").addClass("mask flex-center");
-    });
-});
+
 
 var btn = document.getElementById("myBtn");
 
