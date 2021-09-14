177f5e896787fcec57c41a30c08f965d47c63df1
Shawn9717 <shawn.njoga@student.moringaschool.com>
2021-09-14 11:55:37 +0300

    add submit

--- javascript/java.js
+++ javascript/java.js
@@ -1,8 +1,8 @@
 //For toggling on WHAT WE DO
 $(document).ready(function() {
     $(".pendo1").click(function() {
-        $("#click1").toggle();
         $("#njoga1").toggle();
+        $("#click1").toggle();
     });
     $(".pendo2").click(function() {
         $("#njoga2").toggle();
@@ -14,33 +14,5 @@ $(document).ready(function() {
     });
 });
 
-// For hover effect on PORTFOLIO
 
-
-var btn = document.getElementById("myBtn");
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function(event) {
-    // Validate form for entry
-    var x = document.forms["myForm"]["contact-name"].value;
-    if (x == "") {
-        alert("Name not entered");
-        return false;
-    }
-    var y = document.forms["myForm"]["contact-email"].value;
-    if (y == "") {
-        alert("Email not entered");
-        return false;
-    }
-    var z = document.forms["myForm"]["contact-message"].value;
-    if (z == "") {
-        alert("You have no message to send");
-        return false;
-    }
-    var person1Input = $("input#contact-name").val();
-    $("span#outputName").append(person1Input);
-
-    $('.toast').toast('show');
-    event.preventDefault();
-}
 
