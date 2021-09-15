3ad181de6b58f0dab60e47a318cc4b06811d7995
Shawn9717 <shawn.njoga@student.moringaschool.com>
2021-09-15 14:41:01 +0300

    initial

--- javascript/java.js
+++ javascript/java.js
@@ -2,19 +2,20 @@
 $(document).ready(function() {
     $(".pendo1").click(function() {
         $("#njoga1").slideToggle();
-        $("#click").slideToggle(slow);
+        $("#click").hide();
     });
     $(".pendo2").click(function() {
         $("#njoga2").slideToggle();
-        $("#click2").slideToggle(slow);
+        $("#click2").hide(fast);
     });
     $(".pendo3").click(function() {
         $("#njoga3").slideToggle();
-        $("#click3").slideToggle(slow);
+        $("#click3").hide(fast);
+    });
+    $(".PORTFOLIO img").hover(function() {
+        $(".col-3 p-4 img").addclass("image");
+        $(".project-name").show();
     });
 });
 //Howver effect on portfolio images
-
-
-
-
+ 
